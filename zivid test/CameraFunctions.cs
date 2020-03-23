ad3b71f3a19d52d5b746d63a276d1fceadb9aeea
alnavski98 <59961638+alnavski98@users.noreply.github.com>
2020-03-23 14:45:09 +0100

    plc more functonality

--- zivid test/CameraFunctions.cs
+++ zivid test/CameraFunctions.cs
@@ -51,17 +51,20 @@ namespace zivid_test
             //if baseline is taken, calculate distance. else don't
             if (zivid_test.Program.f.runBaseline)
             {
+                while (plc.str1 != 1 || plc.str1 != 2)
+                {
+                    if (plc.str1 == 1)
+                    {
+                        blCylinderIn = fileTransferer.readFromFile(blFileNames[0]);
+                        distance = PointCloudHelpers.calculateDistance(pc, blCylinderIn);
+                    }
+                    else if (plc.str1 == 2)
+                    {
+                        blCylinderOut = fileTransferer.readFromFile(blFileNames[1]);
+                        distance = PointCloudHelpers.calculateDistance(pc, blCylinderOut);
+                    }
+                }
                 //var activeBaseline = baselines.Where(t => t.baseLineId.Equals(baselineIdSim)).ToList();
-                if(plc.str1 == 1)
-                {
-                    blCylinderIn = fileTransferer.readFromFile(blFileNames[0]);
-                    distance = PointCloudHelpers.calculateDistance(pc, blCylinderIn);
-                }
-                else if(plc.str1 == 2)
-                {
-                    blCylinderOut = fileTransferer.readFromFile(blFileNames[1]);
-                    distance = PointCloudHelpers.calculateDistance(pc, blCylinderOut);
-                }
                 //FileTransfer.writeCSV(fileName, distance);
                 Console.WriteLine(distance);
                 inc++;
